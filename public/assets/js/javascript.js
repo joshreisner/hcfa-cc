f7e756111ed4b14ade2dda13dd3ab4d114a7397d
Josh Reisner <josh@joshreisner.com>
2015-10-29 08:59:07 -0700

    tinymce fixes per rosemarie

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -25,10 +25,11 @@ tinyMCE.init({
 	editor_deselector : "mceNoEditor",
 	statusbar: false,
 	menubar: false,
+	browser_spellcheck : true,
 	plugins: [
 		"advlist autolink lists link image charmap print preview anchor",
 		"searchreplace visualblocks code fullscreen",
-		"insertdatetime media table contextmenu paste jbimages"
+		"insertdatetime media table paste jbimages"
 	],
 	//toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image jbimages",
 	toolbar: "insertfile undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link jbimages",
